398494fed1af04391ef1e41ad45471e535f84160
Alina Cho <acho@academic.rrc.ca>
2025-09-28 20:38:18 -0500

    Added faker and populated 676 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+puts "Deleting products..."
+Product.destroy_all
+
+puts "Populating 676 products..."
+676.times do
+  Product.create!(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    price: Faker::Commerce.price(range: 1.0..999.0),
+    stock_quantity: Faker::Number.between(from: 0, to: 500)
+  )
+end
+
+puts "Done. Products: #{Product.count}"
